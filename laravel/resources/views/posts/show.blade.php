88600521f8071d388e650b59f3748eaaecc4acd6
programmingmuscle <programmingmuscle@gmail.com>
2020-05-06 15:49:25 +0900

    postsのshow.blade.phpにてコード整頓

--- laravel/resources/views/posts/show.blade.php
+++ laravel/resources/views/posts/show.blade.php
@@ -1,13 +1,10 @@
 @extends ('layouts.mainArea')
 
 @section ('title')
-
    投稿詳細
-
 @endsection
 
 @section ('mainArea_content')
-
     <div class="media show_content">
         <img class="profile-image" src="{{ Gravatar::src($post->user->email), 50}}" alt="ユーザのプロフィール画像です。">
         <div class="media-body">
@@ -21,5 +18,4 @@
 
         </div>
     </div>
-
 @endsection
