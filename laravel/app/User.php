0f6cad350bc221a897312c3476379cd20f22bfd2
programmingmuscle <programmingmuscle@gmail.com>
2020-05-18 21:21:09 +0900

    User.phpにフォロー、フォロワーの、多対多の関係を表すメソッドを記載

--- laravel/app/User.php
+++ laravel/app/User.php
@@ -31,4 +31,39 @@ class User extends Authenticatable
     {
         return $this->hasMany(Post::class);
     }
+
+    public function followings() 
+    {
+        return $this->belongsToMany(User::class, 'user_follow', 'user_id', 'follow_id')->withTimestamps();
+    }
+
+    public function followers() 
+    {
+        return $this->belongsToMany(User::class, 'user_follow', 'follow_id', 'user_id')->withTimestamps();
+    }
+
+    public function is_following($userId) 
+    {
+        return $this->followings()->where('follow_id', $userId)->exists();
+    }
+
+    public function follow($userId) 
+    {
+        $exist = $this->is_following($userId);
+        $its_me = $this->id == $userId;
+
+        if (!$exist && !$its_me) {
+            $this->followings()->attach($userId);
+        }
+    }
+
+    public function unfollow($userId)
+    {
+        $exist = $this->is_following($userId);
+        $its_me = $this->id == $userId;
+
+        if ($exist && !$its_me) {
+            $this->followings()->detach($userId);
+        }
+    }
 }
