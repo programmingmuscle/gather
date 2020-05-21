9f209d755533a23539700cc6797bd0c7fd2784c4
programmingmuscle <programmingmuscle@gmail.com>
2020-05-21 17:14:19 +0900

    user_concernテーブル作成

---
+++ laravel/database/migrations/2020_05_21_170610_create_user_concern_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserConcernTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_concern', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->integer('concern_id')->unsigned()->index();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('concern_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->unique(['user_id', 'concern_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_concern');
+    }
+}
