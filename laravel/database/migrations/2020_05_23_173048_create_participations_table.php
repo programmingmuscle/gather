08360a24351b060571d44bebcb2bd97cde5448da
programmingmuscle <programmingmuscle@gmail.com>
2020-05-23 18:24:47 +0900

    participantsテーブルを削除しparticipationsテーブルを作成

--- laravel/database/migrations/2020_05_23_173048_create_participations_table.php
+++ laravel/database/migrations/2020_05_23_173048_create_participations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateParticipantsTable extends Migration
+class CreateParticipationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateParticipantsTable extends Migration
      */
     public function up()
     {
-        Schema::create('participants', function (Blueprint $table) {
+        Schema::create('participations', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned()->index();
             $table->integer('post_id')->unsigned()->index();
@@ -33,6 +33,6 @@ class CreateParticipantsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('participants');
+        Schema::dropIfExists('participations');
     }
 }
