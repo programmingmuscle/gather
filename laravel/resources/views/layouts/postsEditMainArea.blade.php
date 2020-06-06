9373d838d1eb9c29d35a22d32b122ad2704331c1
programmingmuscle <programmingmuscle@gmail.com>
2020-06-06 21:44:11 +0900

    eApp.blade.php及びeMainArea.blade.phpのファイル名を変更

--- laravel/resources/views/layouts/postsEditMainArea.blade.php
+++ laravel/resources/views/layouts/postsEditMainArea.blade.php
@@ -1,4 +1,4 @@
-@extends ('layouts.eApp')
+@extends ('layouts.postsEditApp')
 
 @section('content')
     <div class="main-area">
@@ -7,10 +7,10 @@
             @yield ('title')
 
         </div>
-        <div class="content">
+        <div class="content" id="content">
             
             @if (count($errors) > 0)
-                <p class="error-message alert alert-danger list-unstyled" role="alert">入力に問題があります。再入力して下さい。</p>
+                <p class="error-message alert alert-danger list-unstyled" role="alert" >入力に問題があります。再入力して下さい。</p>
             @endif
 
             @yield ('mainArea_content')
