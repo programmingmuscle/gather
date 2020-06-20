2592a10d1205e9b8df0bf62c9a9500ec0871ce90
programmingmuscle <programmingmuscle@gmail.com>
2020-06-20 17:52:06 +0900

    アカウント編集画面において選手名、メールアドレスが空欄の場合画面遷移せずエラー表示するよう設定

--- laravel/public/assets/js/users-edit.js
+++ laravel/public/assets/js/users-edit.js
@@ -1,5 +1,39 @@
 $('.u-edit-form').on('submit', (e) => {
+    const name = $('#name').val();
+    const email = $('#email').val();
     const password = $('#password').val();
+
+    if(name === '') {
+        e.preventDefault();
+
+        $('#remove-error-content').remove();
+
+        $('.remove-error-name').remove();
+
+        $('<p class="error-message alert alert-danger list-unstyled" id="remove-error-content" role="alert">入力に問題があります。再入力して下さい。</p>').prependTo('#content');
+        
+        $('<div class="error-target remove-error-name">選手名を入力して下さい。</div>').insertAfter('#name-error');
+
+        $('html, body').animate({ scrollTop: 0 }, 600);
+    } else {
+        $('.remove-error-name').remove();
+    }
+
+    if(email === '') {
+        e.preventDefault();
+
+        $('#remove-error-content').remove();
+
+        $('.remove-error-email').remove();
+
+        $('<p class="error-message alert alert-danger list-unstyled" id="remove-error-content" role="alert">入力に問題があります。再入力して下さい。</p>').prependTo('#content');
+        
+        $('<div class="error-target remove-error-email">メールアドレスを入力して下さい。</div>').insertAfter('#email-error');
+
+        $('html, body').animate({ scrollTop: 0 }, 600);
+    } else {
+        $('.remove-error-email').remove();
+    }
 
     if(password === '') {
         e.preventDefault();
