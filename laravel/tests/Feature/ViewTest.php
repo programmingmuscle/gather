e6f8ec832504fc7a890466b1c4c308b79a7a2c62
programmingmuscle <programmingmuscle@gmail.com>
2020-07-09 06:13:12 +0900

    ログインの有無によりボタン表示の有無が変化することの確認をテストから削除

--- laravel/tests/Feature/ViewTest.php
+++ laravel/tests/Feature/ViewTest.php
@@ -23,7 +23,7 @@ class ViewTest extends TestCase
     // 未ログイン状態における各ページへのアクセスをテスト
     public function testView()
     {
-        // ユーザ作成
+        // ユーザ及び投稿を作成
         $user = factory(User::class)->create();
         $post = factory(Post::class)->create();
 
@@ -39,50 +39,31 @@ class ViewTest extends TestCase
         $response = $this->get('signup');
         $response->assertStatus(200);
 
-        // ユーザ一覧画面(未ログイン時はフォローボタンを表示しない)
+        // ユーザ一覧画面
         $response = $this->get('users');
         $response->assertStatus(200);
-        $response->assertDontSee('フォローする');
-
-        // ユーザ詳細-タイムラインタブ画面(未ログイン時はフォローボタンを表示しない)
+        
+        // ユーザ詳細
         $response = $this->get(route('users.show', ['id' => $user->id]));
         $response->assertStatus(200);
-        $response->assertDontSee('フォローする');
-        
-        // ユーザ詳細-投稿タブ画面(未ログイン時はフォローボタンを表示しない)
-        $response = $this->get(route('users.posts', ['id' => $user->id]));
-        $response->assertStatus(200);
-        $response->assertDontSee('フォローする');
 
-        // ユーザ詳細-参加タブ画面(未ログイン時はフォローボタンを表示しない)
-        $response = $this->get(route('users.participations', ['id' => $user->id]));
-        $response->assertStatus(200);
-        $response->assertDontSee('フォローする');
-
-        // ユーザ詳細-気になるタブ画面(未ログイン時はフォローボタンを表示しない)
-        $response = $this->get(route('users.concerns', ['id' => $user->id]));
-        $response->assertStatus(200);
-        $response->assertDontSee('フォローする');
-
-        // アカウント削除画面(要ログイン 未ログインの場合はログイン画面にリダイレクト)
+        // アカウント削除画面(未ログインの場合はログイン画面にリダイレクト)
         $response = $this->get(route('users.deleteWindow', ['id' => $user->id]));
-        $response->assertStatus(302);
-        $response->assertRedirect('login');
+        $response->assertStatus(302)
+                 ->assertRedirect('login');
 
-        // アカウント編集画面(要ログイン 未ログインの場合はログイン画面にリダイレクト)
+        // アカウント編集画面(未ログインの場合はログイン画面にリダイレクト)
         $response = $this->get(route('users.edit', ['id' => $user->id]));
-        $response->assertStatus(302);
-        $response->assertRedirect('login');
+        $response->assertStatus(302)
+                 ->assertRedirect('login');
 
-        // 投稿一覧画面(未ログイン時は気になるボタンを表示しない)
+        // 投稿一覧画面
         $response = $this->get(route('posts.index'));
         $response->assertStatus(200);
-        $response->assertDontSee('気になる');
 
-        // 投稿詳細(未ログイン時は気になるボタンを表示しない)
+        // 投稿詳細
         $response = $this->get(route('posts.show', ['id' => $post->id]));
         $response->assertStatus(200);
-        $response->assertDontSee('気になる');
 
         // フォロー一覧画面
         $response = $this->get(route('users.followings', ['id' => $user->id]));
@@ -91,56 +72,52 @@ class ViewTest extends TestCase
         // フォロワー一覧画面
         $response = $this->get(route('users.followers', ['id' => $user->id]));
         $response->assertStatus(200);
+        
 
-        // 投稿削除画面(要ログイン 未ログインの場合はログイン画面にリダイレクト)
+        // 投稿削除画面(未ログインの場合はログイン画面にリダイレクト)
         $response = $this->get(route('posts.deleteWindow', ['id' => $post->id]));
-        $response->assertStatus(302);
-        $response->assertRedirect('login');
+        $response->assertStatus(302)
+                 ->assertRedirect('login');
 
-        // 投稿編集画面(要ログイン 未ログインの場合はログイン画面にリダイレクト)
+        // 投稿編集画面(未ログインの場合はログイン画面にリダイレクト)
         $response = $this->get(route('posts.edit', ['id' => $post->id]));
-        $response->assertStatus(302);
-        $response->assertRedirect('login');
+        $response->assertStatus(302)
+                 ->assertRedirect('login');
 
-        // 投稿作成画面(要ログイン 未ログインの場合はログイン画面にリダイレクト)
+        // 投稿作成画面(未ログインの場合はログイン画面にリダイレクト)
         $response = $this->get(route('posts.create', ['id' => $user->id]));
-        $response->assertStatus(302);
-        $response->assertRedirect('login');
+        $response->assertStatus(302)
+                 ->assertRedirect('login');
 
         // 存在しないページへのアクセスには404表示
         $response = $this->get('/no_route');
         $response->assertStatus(404);
     }
 
-    // ログイン状態における各ページへのアクセスをテスト
+    // ログイン状態におけるログインを要するページへのアクセスをテスト
     public function testViewAuth()
     {
-        // ユーザ作成
+        // ユーザ及び投稿を作成
         $user = factory(User::class)->create();
         $post = factory(Post::class)->create();
 
-        // アカウント削除画面(要ログイン)
+        // アカウント削除画面
         $response = $this->actingAs($user)->get(route('users.deleteWindow', ['id' => Auth::id()]));
         $response->assertStatus(200);
 
-        // アカウント編集画面(要ログイン)
+        // アカウント編集画面
         $response = $this->get(route('users.edit', ['id' => Auth::id()]));
         $response->assertStatus(200);
 
-        // ユーザ詳細(ログイン中であっても自身のユーザ詳細ではフォローボタンは表示されない)
-        $response = $this->get(route('users.show', ['id' => Auth::id()]));
-        $response->assertStatus(200);
-        $response->assertDontSee('フォローする');
-
-        // 投稿削除画面(要ログイン)
+        // 投稿削除画面
         $response = $this->get(route('posts.deleteWindow', ['id' => $post->id]));
         $response->assertStatus(200);
 
-        // 投稿編集画面(要ログイン)
+        // 投稿編集画面
         $response = $this->get(route('posts.edit', ['id' => $post->id]));
         $response->assertStatus(200);
 
-        // 投稿作成画面(要ログイン)
+        // 投稿作成画面
         $response = $this->get(route('posts.create', ['id' => Auth::id()]));
         $response->assertStatus(200);
     }
