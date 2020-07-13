4171c9a482ddff8cc8ab67c2aa344559c98baf68
programmingmuscle <programmingmuscle@gmail.com>
2020-07-14 06:33:41 +0900

    投稿の日時項目に終了日時を設けたことに伴い投稿に係るテストに終了日時項目を追加

--- laravel/tests/Feature/PostTest.php
+++ laravel/tests/Feature/PostTest.php
@@ -22,9 +22,10 @@ class PostTest extends TestCase
     // 投稿作成時に係るエラー発生時の挙動をテスト
     public function testPostError()
     {
+        // ユーザを作成
         $user = factory(User::class)->create();
-        $post = factory(Post::class)->create();
 
+        // $userでログインして投稿作成
         $response = $this->actingAs($user)->post(route('posts.store', [
             'title' => '',
             'year' => '',
@@ -32,6 +33,9 @@ class PostTest extends TestCase
             'day' => '',
             'from_hour' => '',
             'from_minute' => '',
+            'to_year' => '',
+            'to_month' => '',
+            'to_day' => '',
             'to_hour' => '',
             'to_minute' => '',
             'place' => '',
@@ -47,8 +51,10 @@ class PostTest extends TestCase
             'people' => '',
         ]));
         
-        $response->assertSessionHasErrors(['title', 'year', 'month', 'day', 'from_hour', 'from_minute', 'to_hour', 'to_minute', 'place', 'address', 'reservation', 'expense', 'ball', 'deadlineYear', 'deadlineMonth', 'deadlineDay', 'deadlineHour','deadlineMinute', 'people']);
+        // 上記投稿作成時の入力必須項目において空欄となっていたものについてセッションにエラーデータが存在していることを確認
+        $response->assertSessionHasErrors(['title', 'year', 'month', 'day', 'from_hour', 'from_minute', 'to_year', 'to_month', 'to_day', 'to_hour', 'to_minute', 'place', 'address', 'reservation', 'expense', 'ball', 'deadlineYear', 'deadlineMonth', 'deadlineDay', 'deadlineHour','deadlineMinute', 'people']);
         
+        // 各項目のエラーに対して正しいエラーメッセージが存在していることを確認
         $this->assertEquals('タイトルを入力して下さい。',
             session('errors')->first('title'));
         $this->assertEquals('開催年を入力して下さい。',
@@ -61,6 +67,12 @@ class PostTest extends TestCase
             session('errors')->first('from_hour'));
         $this->assertEquals('開始時間（分）を入力して下さい。',
             session('errors')->first('from_minute'));
+        $this->assertEquals('終了年を入力して下さい。',
+            session('errors')->first('to_year'));
+        $this->assertEquals('終了月を入力して下さい。',
+            session('errors')->first('to_month'));
+        $this->assertEquals('終了日を入力して下さい。',
+            session('errors')->first('to_day'));
         $this->assertEquals('終了時間を入力して下さい。',
             session('errors')->first('to_hour'));
         $this->assertEquals('終了時間（分）を入力して下さい。',
@@ -87,52 +99,90 @@ class PostTest extends TestCase
             session('errors')->first('deadlineMinute'));
         $this->assertEquals('募集人数を入力して下さい。',
             session('errors')->first('people'));
-
-        
     }
 
+    // 投稿作成に成功するテスト
     public function testPost()
     {
+        // ユーザを作成
         $user = factory(User::class)->create();
-        $post = factory(Post::class)->create();
 
+        // $userでログインして投稿作成
         $response = $this->actingAs($user)->post(route('posts.store', [
-            'title' => 'test',
+            'title' => 'testTitle',
             'year' => date('Y'),
-            'month' => '01',
-            'day' => '01',
-            'from_hour' => '00',
+            'month' => '2',
+            'day' => '1',
+            'from_hour' => '0',
             'from_minute' => '00',
-            'to_hour' => '00',
+            'to_year' => date('Y'),
+            'to_month' => '2',
+            'to_day' => '1',
+            'to_hour' => '1',
             'to_minute' => '00',
-            'place' => 'test',
-            'address' => 'test',
+            'place' => 'testPlace',
+            'address' => 'testAddress',
             'reservation' => '不要',
-            'expense' => 'test',
+            'expense' => 'testExpense',
             'ball' => '軟式',
             'deadlineYear' => date('Y'),
-            'deadlineMonth' => '01',
-            'deadlineDay' => '01',
-            'deadlineHour' => '00',
+            'deadlineMonth' => '1',
+            'deadlineDay' => '1',
+            'deadlineHour' => '0',
             'deadlineMinute' => '00',
             'people' => '1人',
+            'remarks' => 'testRemarks',
         ]));
 
-        $response->assertRedirect(route('users.show', ['id' => Auth::id()]));
-        $response = $this->get(route('posts.index'));
-        $response->assertSee('軟式');
-
+        // データベースに上記投稿が保存されていることを確認
         $this->assertDatabaseHas('posts', [
-            'title' => 'test',
-            'date_time' => date('Y') . '-' . '01' . '-' . '01' . ' ' . '00' . ':' . '00',
-            'end_time' => '00' . ':' . '00',
-            'place' => 'test',
-            'address' => 'test',
+            'title' => 'testTitle',
+            'date_time' => date('Y') . '/' . '2' . '/' . '1' . ' ' . '0' . ':' . '00',
+            'end_time' => date('Y') . '/' . '2' . '/' . '1' . ' ' . '1' . ':' . '00',
+            'place' => 'testPlace',
+            'address' => 'testAddress',
             'reservation' => '不要',
-            'expense' => 'test',
+            'expense' => 'testExpense',
             'ball' => '軟式',
-            'deadline' => date('Y') . '-' . '01' . '-' . '01' . ' ' . '00' . ':' . '00',
+            'deadline' => date('Y') . '/' . '1' . '/' . '1' . ' ' . '0' . ':' . '00',
             'people' => '1人',
+            'remarks' => 'testRemarks',
         ]);
+
+        // 上記投稿を選択して変数に代入
+        $post = Post::first();
+
+        // 投稿作成後に投稿詳細画面にリダイレクトされていることを確認
+        $response->assertRedirect(route('posts.show', ['id' => $post->id]));
+
+        // 投稿詳細画面に上記投稿が表示されることを確認
+        $response = $this->get(route('posts.show', ['id' => $post->id]));
+        $response->assertSee('投稿詳細')
+                 ->assertSee('testTitle')
+                 ->assertSee(date('Y') . '/' . '2' . '/' . '1' . ' ' . '0' . ':' . '00')
+                 ->assertSee(date('Y') . '/' . '2' . '/' . '1' . ' ' . '1' . ':' . '00')
+                 ->assertSee('testPlace')
+                 ->assertSee('testAddress')
+                 ->assertSee('不要')
+                 ->assertSee('testExpense')
+                 ->assertSee('軟式')
+                 ->assertSee(date('Y') . '/' . '1' . '/' . '1' . ' ' . '0' . ':' . '00')
+                 ->assertSee('1人')
+                 ->assertSee('testRemarks');
+
+        // 投稿一覧画面に遷移して上記投稿が表示されることを確認
+        $response = $this->get(route('posts.index'));
+        $response->assertSee('投稿一覧')
+                 ->assertSee('testTitle')
+                 ->assertSee(date('Y') . '/' . '2' . '/' . '1' . ' ' . '0' . ':' . '00')
+                 ->assertSee(date('Y') . '/' . '2' . '/' . '1' . ' ' . '1' . ':' . '00')
+                 ->assertSee('testPlace')
+                 ->assertSee('testAddress')
+                 ->assertSee('不要')
+                 ->assertSee('testExpense')
+                 ->assertSee('軟式')
+                 ->assertSee(date('Y') . '/' . '1' . '/' . '1' . ' ' . '0' . ':' . '00')
+                 ->assertSee('1人')
+                 ->assertSee('testRemarks');
     }
 }
