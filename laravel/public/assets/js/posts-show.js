030b046eeaf1f6cbc645d88be83c7f52cf20aed2
programmingmuscle <programmingmuscle@gmail.com>
2020-07-21 20:52:59 +0900

    【修正】投稿詳細画面におけるメッセージの投稿についてバリデーションを設定
    【修正】javascriptによって画面遷移せずエラーメッセージを表示するよう設定

--- laravel/public/assets/js/posts-show.js
+++ laravel/public/assets/js/posts-show.js
@@ -17,17 +17,20 @@ $('.message-form').on('submit', (e) => {
 
         $('.remove-error-messageContent').remove();
 
+        $('.remove-error-messageContentStr').remove();
+
         $('<p class="error-message alert alert-danger list-unstyled" id="remove-error-content" role="alert">入力に問題があります。再入力して下さい。</p>').prependTo('#content');
         
         $('<div class="error-target remove-error-messageContent">空のメッセージは送信できません。</div>').insertAfter('#messageContent-error');
   
         $('html, body').animate({ scrollTop: 0 }, 600);
-    }
 
-    if (messageContent.length > 191) {
+    }  else if (messageContent.length > 191) {
         e.preventDefault();
 
         $('#remove-error-content').remove();
+
+        $('.remove-error-messageContent').remove();
 
         $('.remove-error-messageContentStr').remove();
 
@@ -36,5 +39,12 @@ $('.message-form').on('submit', (e) => {
         $('<div class="error-target remove-error-messageContentStr">メッセージは191文字以内として下さい。</div>').insertAfter('#messageContent-error');
   
         $('html, body').animate({ scrollTop: 0 }, 600);
+
+    } else {
+        $('#remove-error-content').remove();
+
+        $('.remove-error-messageContent').remove();
+
+        $('.remove-error-messageContentStr').remove();
     }
 });
