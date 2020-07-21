991a889e870186b1077d82940b0a0f4f22ea1b5b
programmingmuscle <programmingmuscle@gmail.com>
2020-07-22 07:08:17 +0900

    【続修正】「参加」「気になる」ボタンを未ログイン時にも表示するよう設定
    前回のコミット時にconcern_button.blade.phpの修正が反映されていなかったため

--- laravel/resources/views/concerns/concern_button.blade.php
+++ laravel/resources/views/concerns/concern_button.blade.php
@@ -1,21 +1,23 @@
 @if ($post->user->id != Auth::id())
-    @if (Auth::check())
-        @if (Auth::user()->is_concerned($post->id))
-            <form method="POST" action="{{ route('concerns.unconcern', ['id' => $post->id]) }}" class="d-inline-block">
-                {!! method_field('delete') !!}
-                {{ csrf_field() }}
-                <input type="submit" value="気になる" class="btn unconcern-button d-inline-block">
-            </form>
+    <div class="button-position ml-3">
+        @if (Auth::check())
+            @if (Auth::user()->is_concerned($post->id))
+                <form method="POST" action="{{ route('concerns.unconcern', ['id' => $post->id]) }}" class="d-inline-block">
+                    {!! method_field('delete') !!}
+                    {{ csrf_field() }}
+                    <input type="submit" value="気になる" class="btn unconcern-button d-inline-block">
+                </form>
+            @else
+                <form method="POST" action="{{ route('concerns.concern', ['id' => $post->id]) }}" class="d-inline-block">
+                    {{ csrf_field() }}
+                    <input type="submit" value="気になる" class="btn concern-button d-inline-block">
+                </form>
+            @endif
         @else
             <form method="POST" action="{{ route('concerns.concern', ['id' => $post->id]) }}" class="d-inline-block">
                 {{ csrf_field() }}
                 <input type="submit" value="気になる" class="btn concern-button d-inline-block">
             </form>
         @endif
-    @else
-        <form method="POST" action="{{ route('concerns.concern', ['id' => $post->id]) }}" class="d-inline-block">
-            {{ csrf_field() }}
-            <input type="submit" value="気になる" class="btn concern-button d-inline-block">
-        </form>
-    @endif
+    </div>
 @endif
