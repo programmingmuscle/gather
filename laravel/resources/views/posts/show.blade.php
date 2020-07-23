b7628877be31a7058b6729f3320fd44a3d5cb0c3
programmingmuscle <programmingmuscle@gmail.com>
2020-07-23 17:13:37 +0900

    投稿詳細画面において参加者が０人の場合はdropdown-toggleを表示せずcolorをデフォルトにする

--- laravel/resources/views/posts/show.blade.php
+++ laravel/resources/views/posts/show.blade.php
@@ -34,7 +34,11 @@
             @include ('concerns.concern_button')
 
             <div class="accordion">
-                <div class="ml-3 mb-3 mt-3 accordion-title dropdown-toggle">参加者<span class="badge badge-primary badge-pill">{{ $count_participate_users }}</span></div>
+                @if (count($users) > 0)
+                    <div class="ml-3 mb-3 mt-3 accordion-title dropdown-toggle">参加者<span class="badge badge-primary badge-pill">{{ $count_participate_users }}</span></div>
+                @else
+                    <div class="ml-3 mb-3 mt-3">参加者<span class="badge badge-secondary badge-pill">{{ $count_participate_users }}</span></div>
+                @endif
 
                 @if (count($users) > 0)
                     <div class="ml-3 accordion-content">
