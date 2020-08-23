0d2536e8eeade0d5eea7e4323e4ad2cad58673ec
programmingmuscle <programmingmuscle@gmail.com>
2020-08-23 17:38:09 +0900

    開始日時、終了日時、及び締切日時をランダムから固定に変更

--- laravel/database/factories/PostFactory.php
+++ laravel/database/factories/PostFactory.php
@@ -8,15 +8,15 @@ $factory->define(App\Post::class, function (Faker $faker) {
             return factory(App\User::class)->create()->id;
         },
         'title' => $faker->text($faker->numberBetween(10,191)),
-        'date_time' => $faker->randomElement([date('Y'), date('Y')+1]) . '/' . $faker->month . '/' . $faker->dayOfMonth . ' ' . $faker->time,
-        'end_time' => $faker->randomElement([date('Y'), date('Y')+1]) . '/' . $faker->month . '/' . $faker->dayOfMonth . ' ' . $faker->time,
+        'date_time' => '2020-01-02 00:00:00',
+        'end_time' => '2020-01-02 00:30:00',
         'place' => $faker->locale,
         'address' => $faker->address,
         'reservation' => $faker->randomElement(['不要', '予約済み', '未予約']),
         'expense' => $faker->text($faker->numberBetween(10,191)),
         'ball' => $faker->randomElement(['軟式', '硬式']),
-        'deadline' => $faker->randomElement([date('Y'), date('Y')+1]) . '/' . $faker->month . '/' . $faker->dayOfMonth . ' ' . $faker->time,
-        'people' => $faker->randomElement(['1人', '2人', '3人', '4人', '5人', '6人', '7人', '8人', '9人', '10人', '11人', '12人', '13人', '14人', '15人', '16人', '17人', '18人', '19人', '20人', '21人', '22人', '23人', '24人', '25人', '26人', '27人', '28人', '29人', '30人', '31人', '32人', '33人', '34人', '35人', '36人', '37人', '38人', '39人', '40人']),
+        'deadline' => '2020-01-01 00:00:00',
+        'people' => $faker->randomElement(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40']),
         'remarks' => $faker->text($faker->numberBetween(10,191)),
     ];
 });
