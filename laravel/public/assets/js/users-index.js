700182ed50cd3d506918ad071200641bb8bb8fec
programmingmuscle <programmingmuscle@gmail.com>
2020-09-09 21:42:33 +0900

    選手一覧画面にてフォロー機能をajax通信にて実施できるよう設定

--- laravel/public/assets/js/users-index.js
+++ laravel/public/assets/js/users-index.js
@@ -3,3 +3,149 @@ $('.detail').on('click', (e) => {
     console.log(url);
     window.location.href = url;
 });
+
+$(function() {
+    follow_data();
+});
+
+function follow_data() {
+    $.ajaxSetup({
+        headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}
+    });
+    
+    $('.follow-button-ajax').on('click', (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        let userId = $(e.currentTarget).parent().parent().attr('data-userId');
+        console.log(userId);
+        console.log(e.currentTarget);
+        let html = `
+            <form class="d-inline-block">
+                <input type="submit" value="フォロー中" class="btn unfollow-button unfollow-button-ajax-document d-inline-block">
+            </form>
+        `;
+
+        $.ajax({
+            type: 'POST',
+            url: "/result/ajax/" + userId + "/follow",
+            success: () => {
+                $(e.currentTarget).parent().parent().prepend(html);
+                $(e.currentTarget).parent().remove();
+            },
+            error: (jqXHR, textStatus, errorThrown) => {
+                alert("データの取得に失敗しました。");
+                console.log("ajax通信に失敗しました");
+                console.log("jqXHR          : " + jqXHR.status);
+                console.log("textStatus     : " + textStatus);
+                console.log("errorThrown    : " + errorThrown.message);
+                console.log("URL            : " + url);
+            }
+        });        
+    });
+
+    $('.button-position').on('click', '.follow-button-ajax-document', (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        let userId = $(e.currentTarget).parent().parent().attr('data-userId');
+        let eCurrentTargetParent = $(e.currentTarget).parent();
+        let eCurrentTargetParentParent = $(e.currentTarget).parent().parent();
+        let htmlDocument = `
+            <form class="d-inline-block">
+                <input type="submit" value="フォロー中" class="btn unfollow-button unfollow-button-ajax-document d-inline-block">
+            </form>
+        `;
+        console.log(htmlDocument);
+
+        $.ajax({
+            type: 'POST',
+            url: "/result/ajax/" + userId + "/follow",
+            success: () => {
+                console.log(e.currentTarget);
+                $(eCurrentTargetParentParent).prepend(htmlDocument);
+                $(eCurrentTargetParent).remove();
+            },
+            error: (jqXHR, textStatus, errorThrown) => {
+                alert("データの取得に失敗しました。");
+                console.log("ajax通信に失敗しました");
+                console.log("jqXHR          : " + jqXHR.status);
+                console.log("textStatus     : " + textStatus);
+                console.log("errorThrown    : " + errorThrown.message);
+                console.log("URL            : " + url);
+            }
+        });
+    });
+}
+
+$(function() {
+    unfollow_data();
+});
+
+function unfollow_data() {
+    $.ajaxSetup({
+        headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}
+    });
+    
+    $('.unfollow-button-ajax').on('click', (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        let userId = $(e.currentTarget).parent().parent().attr('data-userId');
+        console.log(userId);
+        console.log(e.currentTarget);
+        let html = `
+            <form method="POST" class="d-inline-block">
+                <input type="submit" value="フォロー" class="btn follow-button follow-button-ajax-document d-inline-block">
+            </form>
+        `;
+
+        $.ajax({
+            type: 'POST',
+            url: "/result/ajax/" + userId + "/unfollow",
+            success: () => {
+                console.log(e.currentTarget);
+                $(e.currentTarget).parent().parent().prepend(html);
+                $(e.currentTarget).parent().remove();
+            },
+            error: (jqXHR, textStatus, errorThrown) => {
+                alert("データの取得に失敗しました。");
+                console.log("ajax通信に失敗しました");
+                console.log("jqXHR          : " + jqXHR.status);
+                console.log("textStatus     : " + textStatus);
+                console.log("errorThrown    : " + errorThrown.message);
+                console.log("URL            : " + url);
+            }
+        });
+    });
+
+    $('.button-position').on('click', '.unfollow-button-ajax-document', (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        let userId = $(e.currentTarget).parent().parent().attr('data-userId');
+        let eCurrentTargetParent = $(e.currentTarget).parent();
+        let eCurrentTargetParentParent = $(e.currentTarget).parent().parent();
+        console.log(userId);
+        let htmlDocument = `
+            <form class="d-inline-block">
+                <input type="submit" value="フォロー" class="btn follow-button follow-button-ajax-document d-inline-block">
+            </form>
+        `;
+        console.log(htmlDocument);
+
+        $.ajax({
+            type: 'POST',
+            url: "/result/ajax/" + userId + "/unfollow",
+            success: () => {
+                console.log(eCurrentTargetParentParent);
+                $(eCurrentTargetParentParent).prepend(htmlDocument);
+                $(eCurrentTargetParent).remove();
+            },
+            error: (jqXHR, textStatus, errorThrown) => {
+                alert("データの取得に失敗しました。");
+                console.log("ajax通信に失敗しました");
+                console.log("jqXHR          : " + jqXHR.status);
+                console.log("textStatus     : " + textStatus);
+                console.log("errorThrown    : " + errorThrown.message);
+                console.log("URL            : " + url);
+            }
+        });
+    });
+}
--- laravel/resources/views/user_follow/follow_button.blade.php
+++ laravel/resources/views/user_follow/follow_button.blade.php
@@ -1,10 +1,10 @@
 @if (Auth::check() && ($user->id != Auth::id()))
     @if (Auth::user()->is_following($user->id))
-        <form method="POST" class="d-inline-block">
+        <form class="d-inline-block">
             <input type="submit" value="フォロー中" class="btn unfollow-button unfollow-button-ajax d-inline-block">
         </form>
     @else
-        <form method="POST" class="d-inline-block">
+        <form class="d-inline-block">
             <input type="submit" value="フォロー" class="btn follow-button follow-button-ajax d-inline-block">
         </form>
     @endif
