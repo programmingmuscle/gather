858509c4bff07661a49f28adb84b93640d441c82
programmingmuscle <programmingmuscle@gmail.com>
2020-10-18 16:38:38 +0900

    users.show.blade.phpの参加ボタンのコードをparticipatins.commonで置き換え

--- laravel/resources/views/users/show.blade.php
+++ laravel/resources/views/users/show.blade.php
@@ -99,6 +99,7 @@
 
 								@if (Auth::check() && ($post->user->id != Auth::id()))
 									<div class="button-position ml-3">
+
 										@if (Auth::user()->is_participating($post->id))
 											<form method="POST" action="{{ route('participations.cancel', ['id' => $post->id]) }}" class="d-inline-block">
 												{!! method_field('delete') !!}
@@ -106,23 +107,9 @@
 												<input type="submit" value="参加中" class="btn cancel-button d-inline-block">
 											</form>
 										@else
-											@if (($post->people > $post->participate_users()->count()) && ($post->deadline > $now))
-												<form method="POST" action="{{ route('participations.participate', ['id' => $post->id]) }}" class="d-inline-block">
-													{{ csrf_field() }}
-													<input type="submit" value="参加する" class="btn participate-button d-inline-block">
-												</form>
-											@elseif (($post->people <= $post->participate_users()->count()) && ($post->deadline <= $now))
-												<p class="full-note">・定員到達</p>
-												<p class="deadline-note">・応募期間終了</p>
-												<button class="participate-button-full">参加する</button>
-											@elseif (($post->people <= $post->participate_users()->count()) && ($post->deadline > $now))
-												<p class="full-note">・定員到達</p>
-												<button class="participate-button-full">参加する</button>
-											@else
-												<p class="deadline-note">・応募期間終了</p>
-												<button class="participate-button-full">参加する</button>
-											@endif
+											@include ('participations.common')
 										@endif
+										
 									</div>
 								@endif
 
